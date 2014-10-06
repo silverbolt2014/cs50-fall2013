9d3fd0050698ba18cf30d1f53dea2baa3cd0aa5c
Silver Bolt <silverbolt2014@gmail.com>
2014-10-06 14:50:46 +0000

    Added a template C file for the psets

---
+++ pset/template-file.c
@@ -0,0 +1,9 @@
+// Uncomment if running on CS50 appliance
+// #define RUN_ON_APPLIANCE
+#ifdef RUN_ON_APPLIANCE
+    #include <cs50.h>
+#else
+    #include "../../../cs50lib/cs50.h"
+#endif
+
+int main(int argc, string argv[])
