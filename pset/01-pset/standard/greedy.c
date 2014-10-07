8dce10bdf165dfab036912aa8f8bac906b5f66f4
Silver Bolt <silverbolt2014@gmail.com>
2014-10-07 07:22:44 -0400

    Testing astyle formatting

--- pset/01-pset/standard/greedy.c
+++ pset/01-pset/standard/greedy.c
@@ -10,9 +10,9 @@ This is CS50 Pset1 Fall 2013 - Greedy Problem
 // Uncomment if running on CS50 appliance
 // #define RUN_ON_APPLIANCE
 #ifdef RUN_ON_APPLIANCE
-    #include <cs50.h>
+#include <cs50.h>
 #else
-    #include "../../../cs50lib/cs50.h"
+#include "../../../cs50lib/cs50.h"
 #endif
 
 #include <stdio.h>
@@ -24,67 +24,67 @@ This is CS50 Pset1 Fall 2013 - Greedy Problem
 
 int main(void)
 {
-	// Prompt user for input and validate input
-	float input = -1;
+    // Prompt user for input and validate input
+    float input = -1;
 
-	printf("O hai How much change is owed?\n");
-	input = GetFloat();
+    printf("O hai How much change is owed?\n");
+    input = GetFloat();
 
-	while(input <= 0)
-	{
-		printf("How much change is owed?\n");
-		input = GetFloat();
-	}
+    while(input <= 0)
+    {
+        printf("How much change is owed?\n");
+        input = GetFloat();
+    }
 
-	// Convert from dollars and cents to cents only
-	double inputAsCents = round(input * 100);
-	
-	int cents = (int) inputAsCents;
-	int originalAmount = cents;
-	//printf("originalAmount in cents = %d\n", originalAmount);
+    // Convert from dollars and cents to cents only
+    double inputAsCents = round(input * 100);
 
-	// Determine minimum number of coins needed
-	int numQuarters = 0;
-	int numDimes = 0;
-	int numNickels = 0;
-	int numPennies = 0;
+    int cents = (int) inputAsCents;
+    int originalAmount = cents;
+    //printf("originalAmount in cents = %d\n", originalAmount);
+
+    // Determine minimum number of coins needed
+    int numQuarters = 0;
+    int numDimes = 0;
+    int numNickels = 0;
+    int numPennies = 0;
 
 #ifdef VERSION_1
-	while (cents >= 25)
-	{
-		cents = cents - 25;
-		numQuarters++;
-	}
+    while (cents >= 25)
+    {
+        cents = cents - 25;
+        numQuarters++;
+    }
 
-	while (cents >= 10)
-	{
-		cents = cents - 10;
-		numDimes++;
-	}
+    while (cents >= 10)
+    {
+        cents = cents - 10;
+        numDimes++;
+    }
 
-	while (cents >= 5)
-	{
-		cents = cents - 5;
-		numNickels++;
-	}
-  
-  numPennies = cents;
+    while (cents >= 5)
+    {
+        cents = cents - 5;
+        numNickels++;
+    }
+
+    numPennies = cents;
 
 #endif
 
 #ifdef VERSION_2
-  numQuarters = cents / 25;
-  cents = cents % 25;
-  
-  numDimes = cents / 10;
-  cents = cents % 10;
-  
-  numNickels = cents / 5;
-  numPennies = cents % 5;
+    numQuarters = cents / 25;
+    cents = cents % 25;
+
+    numDimes = cents / 10;
+    cents = cents % 10;
+
+    numNickels = cents / 5;
+    numPennies = cents % 5;
 #endif
 
-	// printf("You will need %d quarters, %d dimes, %d nickels, %d pennies\n", numQuarters, numDimes, numNickels, numPennies);
-	printf("%d\n", numQuarters + numDimes + numNickels + numPennies);
+    // printf("You will need %d quarters, %d dimes, %d nickels, %d pennies\n", numQuarters, numDimes, numNickels, numPennies);
+    printf("%d\n", numQuarters + numDimes + numNickels + numPennies);
 
 
 }
